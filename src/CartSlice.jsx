5572d32109cada35d93b4456ff5d7ea5596cae20
AtteberryShane <shanecatteberry@gmail.com>
2026-06-12 15:53:13 -0500

    Added cart slice reducer functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload; // Destructure payload to get item name
+      // Check if item is already in the cart
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        // If true, increase its quantity
+        existingItem.quantity++;
+      } else {
+        // If false, add it to cart with quantity 1
+        state.items.push({ ...newItem, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      // Remove item from cart based on its name
+      state.items = state.items.filter(item => item.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
+      const { name, quantity } = action.payload; // Destructure payload to get item name and quantity
+      // Find the item in the cart and update its quantity
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     
     },
   },
